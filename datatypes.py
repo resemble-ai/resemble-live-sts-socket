97244f42e3f4552ac9941a7ee8d241469a4189c0
Ket <33789207+Kettukaa@users.noreply.github.com>
2024-08-13 14:20:23 -0700

    Update datatypes.py

--- datatypes.py
+++ datatypes.py
@@ -9,6 +9,7 @@ class AudioData(TypedDict):
     timestamp: int      # milliseconds
     audio_data: bytes   # packed little-endian short integers
 class VoiceSettings(TypedDict):
+    voice: str                 # The name of the desired voice
     crossFadeOffsetRate: float # 0.0 - 1.0
     crossFadeEndRate: float    # 0.0 - 1.0 
     crossFadeOverlapSize: int  # 2048
